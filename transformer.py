9d2919548a68a8c9c8ff2629a854206e898d2939
asharron <alexsharron@gmail.com>
2019-03-23 12:11:58 -0400

    Added pipline for numerical features

--- transformer.py
+++ transformer.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 import os
 import numpy as np
 import hashlib
@@ -37,6 +40,12 @@ housingData = readData()
 dataTrain, dataTest = splitData(housingData, .20, 'Id')
 numAttr = getAttributes(dataTrain, excludeTypes=["object"])
 catAttr = getAttributes(dataTrain, excludeTypes=["int64", "float64"])
-print("Number Attributes", numAttr)
-print("\n")
-print("Categorical Attributes", catAttr)
+
+#Steps to create the numerical pipeline
+numPipeline = Pipeline([
+    ('selector', DataFrameSelector(numAttr)),
+    ('imputer', SimpleImputer(strategy="median")),
+    ('std_scaler', StandardScaler())
+])
+
+numDf = numPipeline.fit_transform(dataTrain)
