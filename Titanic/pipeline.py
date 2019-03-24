2cf5a0aa94982a63b25b6071dcc6c7ab96ea797d
asharron <alexsharron@gmail.com>
2019-03-23 20:27:14 -0400

    Created basic train / test split

--- Titanic/pipeline.py
+++ Titanic/pipeline.py
@@ -1,19 +1,35 @@
 import pandas as pd
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attributes=[]):
+        self.attributes = attributes
+        return self
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attributes].values
 
 def readData(filename):
     return pd.read_csv(filename)
 
+def dropData(data, toDrop=[]):
+    data = data.drop(columns=toDrop)
+    return data
+
 dataset = readData("train.csv")
 
-dataSize = dataset.shape[0]
+dropList = ["Cabin", "Ticket", "Name", "PassengerId"]
+dataset = dropData(data=dataset, toDrop=dropList)
+numCols = dataset.shape[1]
 
-np.random.RandomState(42)
-shuffledIndicies = np.random.permutation(np.arrage(dataSize))
+np.random.seed(42)
 
-testRatio = .2
-trainRatio = 1 - testRatio
-trainStart = (dataSize * trainRatio) - 1
-
-dataTrain = shuffledIndicies[]
-labelsTrain = 
+#Split the datset into train and test, then into features and labels
+dataTrain, dataTest = train_test_split(dataset, test_size=.2)
+labelsTrain = dataTrain.iloc[:, 0:0]
+dataTrain = dataTrain.iloc[:, 1:]
+labelsTest = dataTest.iloc[:, 0:0]
+dataTest = dataTest.iloc[:, 1:]
